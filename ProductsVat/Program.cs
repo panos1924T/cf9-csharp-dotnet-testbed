4d003d4573be6ad219e917fdd067b8db0a431ec3
PanTs <pants.ath@gmail.com>
2026-03-10 20:55:32 +0200

    feat: add find price's vat app

---
+++ ProductsVat/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ProductsVat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double VAT = 0.24;
+            double price = 0.0;
+            double priceVat = 0.0;
+
+            Console.WriteLine("Please type the price you want to calculate the VAT from: ");
+
+            if (!double.TryParse(Console.ReadLine(), out price) || price < 0)
+                Console.WriteLine("Invalid input! ");
+
+            priceVat = price * VAT;
+            price = price + priceVat;
+
+            Console.WriteLine($"The price of: {price} has a VAT of: {priceVat}");
+            
+        }
+    }
+}
