66f0a3b821aec08a502be18956acda7500183083
PanTs <pants.ath@gmail.com>
2026-03-11 17:12:29 +0200

    feat: convert int types to double

--- PayDay/Program.cs
+++ PayDay/Program.cs
@@ -4,18 +4,18 @@
     {
         static void Main(string[] args)
         {
-            const int MONTHLY_WORKING_DAYS = 22;
-            const int DAILY_WORKING_HOURS = 8;
-            const int HOURLY_WORKING_MINUTES = 60;
+            const double MONTHLY_WORKING_DAYS = 22;
+            const double DAILY_WORKING_HOURS = 8;
+            const double HOURLY_WORKING_MINUTES = 60;
 
-            int payByDay = 0;
-            int payByHour = 0;
-            int payByMinute = 0;
+            double payByDay = 0;
+            double payByHour = 0;
+            double payByMinute = 0;
 
-            int salary = 0;
+            double salary = 0;
 
             Console.WriteLine("Please type your salary: ");
-            if(!int.TryParse(Console.ReadLine(), out salary) || salary <= 0)
+            if(!double.TryParse(Console.ReadLine(), out salary) || salary <= 0)
             {
                 Console.WriteLine("Your salary must be a positive integer number! ");
                 return;
@@ -26,9 +26,9 @@
             payByMinute = payByHour / HOURLY_WORKING_MINUTES;
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine($"Your salary's: {salary} \u20AC pay by day is: {payByDay} \u20AC");
-            Console.WriteLine($"Your salary's: {salary} \u20AC pay by hour is: {payByHour} \u20AC");
-            Console.WriteLine($"Your salary's: {salary} \u20AC pay by minute is: {payByMinute} \u20AC");
+            Console.WriteLine($"Your salary's: {salary} \u20AC pay by day is: {payByDay:N2} \u20AC");
+            Console.WriteLine($"Your salary's: {salary} \u20AC pay by hour is: {payByHour:N2} \u20AC");
+            Console.WriteLine($"Your salary's: {salary} \u20AC pay by minute is: {payByMinute:N2} \u20AC");
         }
         
     }
